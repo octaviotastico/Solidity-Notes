659020ef91b61de79ca2f6633b6288ac2eed0231
octaviotastico <octaviopercivaldi2@gmail.com>
2021-08-25 03:10:51 -0300

    Created more functions, and documented them all

--- contracts/NotesContract.sol
+++ contracts/NotesContract.sol
@@ -15,21 +15,29 @@ contract NotesContract {
     string title;
     string content;
 
+    address owner;
     uint createdAt;
     uint updatedAt;
   }
 
   mapping (uint => Note) public notes;
 
+
+  /*
+  * @dev Creates a new note and inserts it in the notes map.
+  * @param title: Title of the note.
+  * @param content: Content of the note.
+  */
   function createNote(string memory _title, string memory _content) public returns (uint) {
     Note memory note = Note({
       id: notesNextID,
       title: _title,
       content: _content,
+
+      owner: msg.sender,
       createdAt: block.timestamp,
       updatedAt: block.timestamp
     });
-
     notes[notesNextID] = note;
 
     notesCount++;
@@ -38,10 +46,21 @@ contract NotesContract {
     return note.id;
   }
 
+
+  /*
+  * @dev Returns the note with the given id.
+  * @param id: ID of the note.
+  */
   function getNote(uint _id) public view returns (string memory, string memory) {
     return (notes[_id].title, notes[_id].content);
   }
 
+
+  /*
+  * @dev Updates the title of a note by the one given by parameter
+  * @param id: ID of the note.
+  * @param title: New title of the note.
+  */
   function updateTitle(uint _id, string memory _title) public view returns (uint) {
     Note memory note = notes[_id];
     note.title = _title;
@@ -49,6 +68,12 @@ contract NotesContract {
     return note.id;
   }
 
+
+  /*
+  * @dev Updates the content of a note by the content given by parameter
+  * @param id: ID of the note.
+  * @param content: New content of the note.
+  */
   function updateContent(uint _id, string memory _content) public view returns (uint) {
     Note memory note = notes[_id];
     note.content = _content;
@@ -56,10 +81,81 @@ contract NotesContract {
     return note.id;
   }
 
+
+  /*
+  * @dev Updates the title and content of a note by the ones given by parameters
+  * @param id: ID of the note.
+  * @param title: New title of the note.
+  * @param content: New content of the note.
+  */
   function updateNote(uint _id, string memory _title, string memory _content) public view returns (uint) {
     updateTitle(_id, _title);
     updateContent(_id, _content);
     return _id;
+  }
+
+
+  /*
+  * @dev Deletes a note by the given id.
+  * @param id: ID of the note.
+  */
+  function deleteNote(uint _id) public returns (bool) {
+    if (notes[_id].owner == msg.sender) {
+      delete notes[_id];
+      notesCount--;
+      return true;
+    }
+    return false;
+  }
+
+
+  /*
+  * @dev Returns an array with all the notes so far.
+  */
+  function getNotes() public view returns (Note[] memory) {
+    Note[] memory notesArray = new Note[](notesCount);
+    for (uint i = 0; i < notesCount; i++) {
+      notesArray[i] = notes[i];
+    }
+    return notesArray;
+  }
+
+
+  /*
+  * @dev Returns a filtered array with all the notes of a given owner.
+  */
+  function getNotesByOwner(address _owner) public view returns (Note[] memory) {
+    Note[] memory notesArray = new Note[](notesCount);
+    uint notesArrayIndex = 0;
+    for (uint i = 0; i < notesCount; i++) {
+      if (notes[i].owner == _owner) {
+        notesArray[notesArrayIndex] = notes[i];
+        notesArrayIndex++;
+      }
+    }
+    return notesArray;
+  }
+
+
+  /*
+  * @dev Returns a filtered array with all the notes with a given title.
+  */
+  function getNotesByTitle(string memory _title) public view returns (Note[] memory) {
+    Note[] memory notesArray = new Note[](notesCount);
+    uint notesArrayIndex = 0;
+    for (uint i = 0; i < notesCount; i++) {
+      if (compareStrings(notes[i].title, _title)) {
+        notesArray[notesArrayIndex] = notes[i];
+        notesArrayIndex++;
+      }
+    }
+    return notesArray;
+  }
+
+
+  ///// AUX Functions /////
+  function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
   }
 
 }
